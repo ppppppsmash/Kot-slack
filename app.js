c265c2017577b79f7199a8d046c220b0fcfeacb3
みんみん <mingming@minminnoMacBook-Pro.local>
2023-03-05 21:45:15 +0900

    オプション画面作成&chromeのlocal.storageを利用して、token情報保存処理を作成

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-// SlackのOAuth Access Token
-const slack_app_token = 'xoxb-4883928497479-4911114450865-Dmsv9x6jZdGzMa8WQkEy807E'
+chrome.storage.local.get(['slack_app_token', 'channel_name', 'channel_id'], function(items) {
+  slack_app_token = items.slack_app_token;
+  channel_name = items.channel_name;
+  channel_id = items.channel_id;
 
-// 打刻対象のメッセージ定義
-channel = 'times_pei'
-from = 'Kot-slack'
+  // ボタンパーツの読み込みを1秒待つ
+  setTimeout("set_event_listener()", 1000);
+});
 
 // Reactionでスタンプする絵文字を定義
 workStart = '出社します'
 workFinish = '退勤します'
 remoteWorkStart = 'リモート開始'
 remoteWorkFinish = 'リモート終了'
-
-// 対象チャンネルのIDを定義
-channel_id = 'C04SGT7RJMA'
 
 // payload情報
 function postToSlack(message){
@@ -25,14 +24,14 @@ function postToSlack(message){
   xhr.onload = function() {
     if (xhr.status === 200) {
       const responseData = JSON.parse(xhr.responseText);
-      alert('Can I post to Slack? :' + responseData.ok);
+      console.log(responseData.ok);
     } else {
       console.log('Request failed.  Returned status of ' + xhr.status);
     }
   };
   xhr.send(data);
 
-  window.alert(`メッセージ"${ts}"に"${message}"を送りました。`);
+  window.alert(`メッセージ"${now}"に"${message}"を送りました。`);
 
 }
 
@@ -44,16 +43,13 @@ function comfirmFunc() {
 // KoTのタイムレコーダーページでイベントリスナーを設定
 function set_event_listener(){
 
-    // 出勤ボタン動作
-    var syukkin_elem = document.getElementsByClassName('record-btn-inner record-clock-in');
-    syukkin_elem[0].addEventListener('click', function(){comfirmFunc()}, false);
+  // 出勤ボタン動作
+  var syukkin_elem = document.getElementsByClassName('record-btn-inner record-clock-in');
+  syukkin_elem[0].addEventListener('click', function(){comfirmFunc()}, false);
 
-    // 退勤ボタン動作
-    var taikin_elem = document.getElementsByClassName('record-btn-inner record-clock-out');
-    taikin_elem[0].addEventListener('click', function(){postToSlack(workFinish)}, false);
+  // 退勤ボタン動作
+  var taikin_elem = document.getElementsByClassName('record-btn-inner record-clock-out');
+  taikin_elem[0].addEventListener('click', function(){postToSlack(workFinish)}, false);
 
-    window.alert(`打刻すると${channel}チャンネルにメッセージ"${workStart}"/"${workFinish}"を送信します。`);
+  window.alert(`打刻すると${channel_name}チャンネルにステータスを送信します。`);
 };
-
-// ボタンパーツの読み込みを1秒待つ
-setTimeout("set_event_listener()", 1000);
---
+++ options.js
@@ -0,0 +1,27 @@
+function save() {
+  let slackAppToken = document.getElementById('slack_app_token').value;
+  let channelName = document.getElementById('channel_name').value;
+  let channelId = document.getElementById('channel_id').value;
+
+  chrome.storage.local.set(
+    {
+      'slack_app_token': slackAppToken,
+      'channel_name': channelName,
+      'channel_id': channelId
+    }, function() {}
+  );
+}
+
+function load() {
+  chrome.storage.local.get(['slack_app_token', 'channel_name', 'channel_id'], function(items) {
+    document.getElementById('slack_app_token').value = items.slack_app_token;
+    document.getElementById('channel_name').value = items.channel_name;
+    document.getElementById('channel_id').value = items.channel_id;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', load);
+document.getElementById('save').addEventListener('click', function() {
+  window.alert('保存しました！');
+  save();
+});
