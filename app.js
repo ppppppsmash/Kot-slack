c5308b3319ba99f75ca075e9bfab8bd38d49ba10
みんみん <mingming@minminnoMacBook-Pro.local>
2023-03-07 23:10:00 +0900

    コードを整理&フロント画面調整

--- app.js
+++ app.js
@@ -1,55 +1,52 @@
-chrome.storage.local.get(['slack_app_token', 'channel_name', 'channel_id'], function(items) {
-  slack_app_token = items.slack_app_token;
-  channel_name = items.channel_name;
-  channel_id = items.channel_id;
+let slackInfo = {};
 
-  // ボタンパーツの読み込みを1秒待つ
-  setTimeout("set_event_listener()", 1000);
+const workStatus = {
+    work_start: '出勤',
+    work_finish: '退勤',
+    remote_start: ':remote_work:',
+    remote_finish: ':remotework_end_owari:'
+};
+
+chrome.storage.local.get(['slack_app_token', 'channel_name', 'channel_id'], function(items) {
+    slackInfo.slack_access_token = items.slack_app_token;
+    slackInfo.channel = items.channel_name;
+    slackInfo.channel_id = items.channel_id;
+
+    // ボタン押した後の読み込みを3秒待てれば
+    setTimeout("postToSlack()", 3000);
 });
 
-// Reactionでスタンプする絵文字を定義
-workStart = '出社します'
-workFinish = '退勤します'
-remoteWorkStart = 'リモート開始'
-remoteWorkFinish = 'リモート終了'
+// payloadとajaxについて
+const payLoad = (message) => {
+    const xhr = new XMLHttpRequest();
+    const url = `https://slack.com/api/chat.postMessage`;
+    const data = `token=${slackInfo.slack_access_token}&channel=${slackInfo.channel_id}&text=${message}`;
+    const current = new Date();
+    const engravingTime = `${current.getFullYear()}年${current.getMonth()+1}月${current.getDate()}日${current.getHours()}時${current.getMinutes()}分`;
 
-// payload情報
-function postToSlack(message){
-  const now = new Date();
-  const xhr = new XMLHttpRequest();
-  const url = `https://slack.com/api/chat.postMessage`;
-  const data = `token=${slack_app_token}&channel=${channel_id}&text=${message}&timestamp=${now}`;
-  xhr.open('POST', url);
-  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      const responseData = JSON.parse(xhr.responseText);
-      console.log(responseData.ok);
-    } else {
-      console.log('Request failed.  Returned status of ' + xhr.status);
-    }
-  };
-  xhr.send(data);
+    xhr.open('POST', url);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.onload = () => {
+        if(xhr.status = 200) {
+            const responseData = JSON.parse(xhr.responseText);
+            console.log(`成功! ${responseData.ok}`);
+        } else {
+            console.log(`ステータス: ${xhr.status}`);
+        }
+    };
+    xhr.send(data);
 
-  window.alert(`メッセージ"${now}"に"${message}"を送りました。`);
+    window.alert(`「${engravingTime}」に${slackInfo.channel}チャンネルに打刻メッセージを送りました。`);
+};
 
-}
-
-function comfirmFunc() {
-  const dialog = window.confirm('今日はリモートワークですか？');
-  return dialog ? postToSlack(remoteWorkStart) : postToSlack(workStart);
-}
-
-// KoTのタイムレコーダーページでイベントリスナーを設定
-function set_event_listener(){
-
-  // 出勤ボタン動作
-  var syukkin_elem = document.getElementsByClassName('record-btn-inner record-clock-in');
-  syukkin_elem[0].addEventListener('click', function(){comfirmFunc()}, false);
-
-  // 退勤ボタン動作
-  var taikin_elem = document.getElementsByClassName('record-btn-inner record-clock-out');
-  taikin_elem[0].addEventListener('click', function(){postToSlack(workFinish)}, false);
-
-  window.alert(`打刻すると${channel_name}チャンネルにステータスを送信します。`);
+// ボタンをクリックしたタイミングで出勤状況を送信させる
+const postToSlack = () => {
+    const clockIn = document.getElementsByClassName('record-btn-inner record-clock-in');
+    clockIn[0].addEventListener('click', () => {
+        payLoad(workStatus.work_start);
+    }, false);
+    const clockOut = document.getElementsByClassName('record-btn-inner record-clock-out');
+    clockOut[0].addEventListener('click', () => {
+        payLoad(workStatus.work_finish);
+    }, false);
 };
--- options.js
+++ options.js
@@ -8,12 +8,12 @@ function save() {
       'slack_app_token': slackAppToken,
       'channel_name': channelName,
       'channel_id': channelId
-    }, function() {}
+    }, () => {}
   );
 }
 
 function load() {
-  chrome.storage.local.get(['slack_app_token', 'channel_name', 'channel_id'], function(items) {
+  chrome.storage.local.get(['slack_app_token', 'channel_name', 'channel_id'], (items) => {
     document.getElementById('slack_app_token').value = items.slack_app_token;
     document.getElementById('channel_name').value = items.channel_name;
     document.getElementById('channel_id').value = items.channel_id;
@@ -21,7 +21,8 @@ function load() {
 }
 
 document.addEventListener('DOMContentLoaded', load);
-document.getElementById('save').addEventListener('click', function() {
+document.getElementById('save').addEventListener('click', () => {
   window.alert('保存しました！');
+  document.getElementById('notice').classList.remove('d-none');
   save();
 });
